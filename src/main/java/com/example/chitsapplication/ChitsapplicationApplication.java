54dcdd7f8130010e4e0777a1089699860d1ececd
91996 <sa>
2026-02-27 16:23:53 +0530

    Initial commit for chits application

---
+++ src/main/java/com/example/chitsapplication/ChitsapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.chitsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitsapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitsapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chitsapplication/ChitsapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chitsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitsapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
